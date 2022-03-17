3077d269d38b1817d4b19af3c7803197905663f9
Hammeedat <hammeedatyusuf12@gmail.com>
2022-03-17 05:23:46 -0700

    positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests functions that prints positive
+ * or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i:
+
+	i = 98;
+	positive or negative(i);
+
+	return (0);
+}
